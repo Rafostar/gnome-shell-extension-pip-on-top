3ca9efd55ad2447e6b17d4ebdf02e178c29857cb
Romanychev <vladimir@romanychev.ru>
2025-05-05 12:38:35 +0300

    Add support yandex browser (Youtube)

--- extension.js
+++ extension.js
@@ -125,6 +125,7 @@ export default class PipOnTop extends Extension
       || window.title == 'Picture in picture'
       || window.title == 'Picture-in-picture'
       || window.title.endsWith(' - PiP')
+      || window.title.endsWith(' - YouTube')
       /* Telegram support */
       || window.title == 'TelegramDesktop');
 
