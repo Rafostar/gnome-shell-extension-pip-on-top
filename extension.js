7820f3f6abfe2b496df1d238a1238b16766ee87f
Y0J1G3N <43652659+Y0J1G3N@users.noreply.github.com>
2025-07-05 03:10:28 +0800

    feat: Add D-Bus interface for controlling window stacking
    - Add D-Bus methods setTopByPid and unsetTopByPid for controlling window stacking
    - Implement sleep/wake handling for D-Bus service stability

--- extension.js
+++ extension.js
@@ -4,7 +4,48 @@
  */
 
 import Meta from 'gi://Meta';
+import Gio from 'gi://Gio';
 import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
+
+const ALWAYS_ON_TOP_DBUS_INTERFACE = `
+<node>
+  <interface name="org.gnome.Shell.Extensions.PipOnTop">
+    <method name="setTopByPid">
+      <arg type="i" name="pid" direction="in"/>
+    </method>
+    <method name="unsetTopByPid">
+      <arg type="i" name="pid" direction="in"/>
+    </method>
+  </interface>
+</node>`;
+
+class DBusImpl {
+    constructor(extension) {
+        this._extension = extension;
+    }
+
+    setTopByPid(pid) {
+        const actors = global.get_window_actors();
+        for (const actor of actors) {
+            const window = actor.meta_window;
+            if (window && window.get_pid() === pid) {
+                this._extension._setTop(window);
+                return;
+            }
+        }
+    }
+
+    unsetTopByPid(pid) {
+        const actors = global.get_window_actors();
+        for (const actor of actors) {
+            const window = actor.meta_window;
+            if (window && window.get_pid() === pid) {
+                this._extension._unsetTop(window);
+                return;
+            }
+        }
+    }
+}
 
 export default class PipOnTop extends Extension
 {
@@ -13,6 +54,9 @@ export default class PipOnTop extends Extension
     this._lastWorkspace = null;
     this._windowAddedId = 0;
     this._windowRemovedId = 0;
+    
+    this._dbusOwnerId = 0;
+    this._dbus = null;
 
     this.settings = this.getSettings();
     this._settingsChangedId = this.settings.connect(
@@ -21,6 +65,104 @@ export default class PipOnTop extends Extension
     this._switchWorkspaceId = global.window_manager.connect_after(
       'switch-workspace', this._onSwitchWorkspace.bind(this));
     this._onSwitchWorkspace();
+    
+    // Set up D-Bus interface
+    this._setupDBus();
+    
+    // Add sleep monitor
+    this._sleepSignalId = Gio.bus_watch_name(
+        Gio.BusType.SYSTEM,
+        'org.freedesktop.login1',
+        Gio.BusNameWatcherFlags.NONE,
+        this._onLoginServiceAppeared.bind(this),
+        null
+    );
+  }
+  
+  _setupDBus() {
+    if (this._dbusOwnerId) {
+        Gio.bus_unown_name(this._dbusOwnerId);
+    }
+    
+    this._dbusOwnerId = Gio.bus_own_name(
+        Gio.BusType.SESSION,
+        'org.gnome.Shell.Extensions.PipOnTop',
+        Gio.BusNameOwnerFlags.NONE,
+        this._onBusAcquired.bind(this),
+        null,
+        this._onNameLost.bind(this)
+    );
+  }
+  
+  _onLoginServiceAppeared() {
+    this._loginProxy = Gio.DBus.system.get_proxy_sync(
+        'org.freedesktop.login1',
+        '/org/freedesktop/login1',
+        'org.freedesktop.login1.Manager',
+        null
+    );
+    
+    this._loginProxy.connectSignal('PrepareForSleep', (proxy, sender, [aboutToSuspend]) => {
+        if (aboutToSuspend) {
+            // System is going to sleep
+            this._teardownDBus();
+        } else {
+            // System is waking up from sleep
+            // A short delay might be needed for the session bus to be ready.
+            setTimeout(() => this._setupDBus(), 1000);
+        }
+    });
+  }
+
+  _teardownDBus() {
+    if (this._dbus) {
+        this._dbus.unexport();
+        this._dbus = null;
+        this._dbusImpl = null;
+    }
+    if (this._dbusOwnerId) {
+        Gio.bus_unown_name(this._dbusOwnerId);
+        this._dbusOwnerId = 0;
+    }
+  }
+
+  _onBusAcquired() {
+      this._dbusImpl = new DBusImpl(this);
+      this._dbus = Gio.DBusExportedObject.wrapJSObject(ALWAYS_ON_TOP_DBUS_INTERFACE, this._dbusImpl);
+      this._dbus.export(Gio.DBus.session, '/org/gnome/shell/extensions/pipontop');
+  }
+
+  _onNameLost() {
+      if (this._dbus) {
+          this._dbus.unexport();
+          this._dbus = null;
+          this._dbusImpl = null;
+      }
+      this._dbusOwnerId = 0;
+  }
+
+  _setTop(window) {
+      if (!window)
+          return;
+
+      window.make_above();
+      window._isForcedOnTop = true;
+      
+      // Apply stick setting if enabled
+      if (this.settings.get_boolean('stick'))
+          window.stick();
+  }
+
+  _unsetTop(window) {
+      if (!window || !window._isForcedOnTop)
+          return;
+
+      window.unmake_above();
+      window._isForcedOnTop = false;
+      
+      // Unstick if it was stuck
+      if (window.on_all_workspaces)
+          window.unstick();
   }
 
   disable()
@@ -34,6 +176,17 @@ export default class PipOnTop extends Extension
       this._lastWorkspace.disconnect(this._windowAddedId);
       this._lastWorkspace.disconnect(this._windowRemovedId);
     }
+
+    // Remove sleep monitor
+    if (this._sleepSignalId) {
+        Gio.bus_unwatch_name(this._sleepSignalId);
+        this._sleepSignalId = 0;
+    }
+    
+    this._loginProxy = null;
+    
+    // Clean up D-Bus
+    this._teardownDBus();
 
     this._lastWorkspace = null;
     this._settingsChangedId = 0;
@@ -52,6 +205,12 @@ export default class PipOnTop extends Extension
             window.unmake_above();
           if (window.on_all_workspaces)
             window.unstick();
+        }
+        
+        // Clean up forced on top windows
+        if (window._isForcedOnTop) {
+          window.unmake_above();
+          window._isForcedOnTop = false;
         }
 
         this._onWindowRemoved(null, window);
@@ -126,9 +285,7 @@ export default class PipOnTop extends Extension
       || window.title == 'Picture-in-picture'
       || window.title.endsWith(' - PiP')
       /* Telegram support */
-      || window.title == 'TelegramDesktop'
-      /* Yandex.Browser support YouTube */
-      || window.title.endsWith(' - YouTube'));
+      || window.title == 'TelegramDesktop');
 
     if (isPipWin || window._isPipAble) {
       let un = (isPipWin) ? '' : 'un';
